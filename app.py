14acb8765b61224686ef8905076f235632735606
ConveningMoon <dj.vergara@hotmail.com>
2023-05-10 13:32:08 +0500

    some changes

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import json
 # Redis connection
 redis_client = redis.Redis(host='localhost', port=6379, db=0)
 
-# Conversion
+# Conversion, on this example 1 dollar = 76 rubles
 RUB_TO_USD = 76
 
 # GET /convert?from=RUR&to=USD&amount=42
