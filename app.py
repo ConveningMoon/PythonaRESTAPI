eaa470b0e71518cd1a039e284c697cbd33c0f631
ConveningMoon <dj.vergara@hotmail.com>
2023-05-11 07:47:25 +0500

    Global conversions getting keys from redis

--- app.py
+++ app.py
@@ -5,27 +5,28 @@ import json
 # Redis connection
 redis_client = redis.Redis(host='localhost', port=6379, db=0)
 
-# Conversion, on this example 1 dollar = 76 rubles
-RUB_TO_USD = 76
-
 # GET /convert?from=RUR&to=USD&amount=42
 async def convert(request):
-    params = request.rel_url.query
-    from_currency = params.get('from')
-    to_currency = params.get('to')
-    amount = float(params.get('amount'))
+    from_currency = request.query.get('from') #RUR
+    to_currency = request.query.get('to') #USD
+    amount = float(request.query.get('amount'))
 
-    if from_currency == 'RUR' and to_currency == 'USD':
-        converted_amount = amount / RUB_TO_USD
-        response = {
-            'from': from_currency,
-            'to': to_currency,
+    # Query Redis to get exchange rate for the given currencies
+    exchange_rate = redis_client.get(f'{from_currency}:{to_currency}') #The key format is Currency1:Currency2
+    if exchange_rate is None:
+        return web.Response(status=400, text='Exchange rate not found')
+
+    else:
+        # Calculate the converted amount
+        converted_amount = amount * float(exchange_rate)
+
+        # Return the result as JSON
+        return web.json_response({
+            'from_currency': from_currency,
+            'to_currency': to_currency,
             'amount': amount,
             'converted_amount': converted_amount
-        }
-        return web.Response(text=json.dumps(response), content_type='application/json')
-    else:
-        return web.Response(text='Conversion not supported', status=400)
+        })
 
 
 # POST /database?merge=1
@@ -52,4 +53,5 @@ app.router.add_get('/convert', convert)
 app.router.add_post('/database', database)
 
 if __name__ == '__main__':
+    print(redis_client.get("RUR:USD"))
     web.run_app(app)
